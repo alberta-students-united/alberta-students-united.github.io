b980398ec7a1421f4e83a1f48ca1f9037447ac52
Hannah Mudge <Heenawter@users.noreply.github.com>
2019-11-02 00:07:08 -0600

    important dates added

--- js/script.js
+++ js/script.js
@@ -12,42 +12,28 @@ $(document).ready(function () {
             title: "MRU Coordination Meeting",
             time: "3:00 PM - 6:00 PM",
             location: "Mount Royal University, EL 1341",
-            description: "Meeting for MRU students to organize. All other schools welcome."
+            description: "Meeting for MRU students to organize. All other schools welcome.",
+            important: false
         },
         {
             date: "11/7/2019",
             title: "Prep for General Assembly",
             time: "4:00 PM - 6:00 PM",
             location: "University of Calgary, TFDL 210",
-            description: "We will provide updates for our current status and discuss the goals of the upcoming general assembly."
+            description: "We will provide updates for our current status and discuss the goals of the upcoming general assembly.",
+            important: false
+            
         },
         {
             date: "11/14/2019",
             title: "General Assembly",
             time: "4:00 PM - 6:00 PM",
             location: "University of Calgary, Arts Lounge",
-            description: "General assembly to gather support numbers and barnstorm. "
+            description: "General assembly to gather support numbers and barnstorm. ",
+            important: true
         }
     ]
 
-    // <div class="row row-striped">
-    //     <div class="col-2 text-right">
-    //         <h1 class="display-4"><span class="badge badge-secondary">06</span></h1>
-    //         <h2>NOV</h2>
-    //     </div>
-    //     <div class="col-10">
-    //         <h3 class="text-uppercase"><strong>MRU Coordination Meeting</strong></h3>
-    //         <ul class="list-inline">
-    //             <li class="list-inline-item"><i class="fa fa-calendar" aria-hidden="true"></i> Wednesday </li>
-    //             <li class="list-inline-item"><i class="fa fa-clock" aria-hidden="true"></i> 3:00 PM - 6:00 PM</li>
-    //             <li class="list-inline-item"><i class="fa fa-map-marker-alt" aria-hidden="true"></i> Mount Royal University, EL 1341</li>
-    //         </ul>
-    //         <p>
-    //             Meeting for MRU students to organize. All other schools welcome.
-    //         </p>
-    //     </div>
-    // </div>
-    
     var calendar = document.getElementById("calendar-content");
     var temp = document.getElementsByTagName("template")[0];
     var currentEvent;
@@ -81,6 +67,10 @@ $(document).ready(function () {
         location.append(events[i].location);
         description.append(events[i].description);
 
+        if(events[i].important) {
+            date_num.classList.add("important-event");
+        }
+
         date_num.id = "date-num-" + i;
         date_month.id = "date-month-" + i;
         date_dayOfTheWeek.id = "dotw-" + i;
@@ -91,69 +81,4 @@ $(document).ready(function () {
 
         calendar.appendChild(currentEvent);
     }
-
-    // var row;
-    // var left_column;
-    // var right_column;
-    // var h1;
-    // var h2;
-    // var day_span;
-    // var title;
-    // var ul;
-    // var description;
-    // var dayOfTheWeek;
-    // var location;
-    // var time;
-    // var calendarIcon;
-    // var clockIcon;
-    // var locationIcon;
-    // for (i = 0; i < events.length; i++) {
-    //     row = document.createElement("div");
-    //     left_column = document.createElement("div");
-    //     right_column = document.createElement("div");
-    //     h1 = document.createElement("h1");
-    //     h2 = document.createElement("h2");
-    //     day_span = document.createElement("span");
-    //     title = document.createElement("h3");
-    //     ul = document.createElement("ul");
-    //     description = document.createElement("p");
-
-    //     row.className = "row row-striped";
-    //     left_column.className = "rcol-2 text-right";
-    //     right_column.className = "col-10";
-    //     h1.className = "display-4"
-    //     day_span.className = "badge badge-secondary";
-    //     title.className = "text-uppercase";
-    //     ul.className = "list-inline";
-
-    //     row.appendChild(left_column);
-    //     row.appendChild(right_column);
-    //     left_column.appendChild(h1);
-    //     left_column.appendChild(h2);
-    //     h1.appendChild(day_span);
-    //     right_column.appendChild(title);
-    //     right_column.appendChild(ul);
-    //     right_column.appendChild(description);
-
-    //     calendarIcon = document.createElement("i");
-    //     calendarIcon.className = "fa fa-calendar";
-    //     clockIcon = document.createElement("i");
-    //     clockIcon.className = "fa fa-clock";
-    //     locationIcon = document.createElement("i");
-    //     locationIcon.className = "fa fa-map-marker-alt";
-
-    //     calendar.appendChild(row);
-
-    //     dayOfTheWeek = document.createElement("li");
-    //     dayOfTheWeek.className = "list-inline-item";
-    //     dayOfTheWeek.appendChild(calendarIcon);
-    //     dayOfTheWeek.append(" Wednesday");
-    //     ul.appendChild(dayOfTheWeek);
-
-    //     location = document.createElement("li");
-    //     location.className = "list-inline-item";
-    //     location.appendChild(locationIcon);
-    //     location.append(events[i].location);
-    //     ul.appendChild(location);
-    // }
 });
